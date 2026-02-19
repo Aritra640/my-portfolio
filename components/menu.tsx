147aad287b19861eedb623acbf3e9a3dc068fb3e
Aritra640 <chatterjeearitra15@gmail.com>
2026-02-19 23:13:40 +0530

    minor changes

--- components/menu.tsx
+++ components/menu.tsx
@@ -1,9 +1,26 @@
 "use client";
 
-export function Menu(){
-
-  return <div className="flex">
+import SearchIcon from "./icons/search";
 
 
-  </div>
+export function Menu() {
+  return (
+    <div className="flex justify-around pt-2 md:pt-4">
+      <button className="btn btn-outline btn-primary border-purple-900 rounded-2xl hover:bg-purple-950">
+        home
+      </button>
+      <button className="btn btn-outline btn-primary border-purple-900 rounded-2xl hover:bg-purple-950">
+        projects
+      </button>
+      <button className="btn btn-outline btn-primary border-purple-900 rounded-2xl hover:bg-purple-950">
+        notes
+      </button>
+      <button className="btn btn-outline btn-primary border-purple-900 rounded-2xl hover:bg-purple-950">
+        contact
+      </button>
+      <button className="btn btn-outline btn-primary border-purple-900 rounded-2xl hover:bg-purple-950">
+        <SearchIcon />
+      </button>
+    </div>
+  );
 }
